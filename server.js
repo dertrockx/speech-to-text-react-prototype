df4a448e18be523d07d550caa6c391528c12fc13
Ian Salazar <iisalazar@up.edu.ph>
2019-12-21 14:12:07 +0800

    updated server file

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.resolve(__dirname, './build')));
-app.get('/.well-known/acme-challenge/KkF72TfZq0TO47vj8AR0h3ey-8iWLFuB6DfJ3SxoGBM', (req, res) => {
-    res.send('KkF72TfZq0TO47vj8AR0h3ey-8iWLFuB6DfJ3SxoGBM.vHLn0LStE7CnyM_FasQhpmNRI9rK8uiwpFN6TcUF3Ck');
+app.get('/.well-known/acme-challenge/aR8pzSmsDD5-u4rbd_GiDLb47XOz3JCeYGvs2b3fFwA', (req, res) => {
+    res.send('aR8pzSmsDD5-u4rbd_GiDLb47XOz3JCeYGvs2b3fFwA.vHLn0LStE7CnyM_FasQhpmNRI9rK8uiwpFN6TcUF3Ck');
 })
 
 app.get('*', function(request, response) {
